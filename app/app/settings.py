4e498d62539f0c42b86142a6365f5b0368f5e26c
Nathán <n.mtz.cas@gmail.com>
2020-10-07 21:06:22 -0500

    Authentication added

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'a)9h*+9l8%*+mpkvw$go*q@a_4wbt5)p*rt1_1p7lotk1he8y+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
     'core',
+    'user',
 ]
 
 MIDDLEWARE = [
